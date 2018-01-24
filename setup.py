cc5aac97a11688ab55aedae523129fc3c4268850
Andrew Rabert <andrewr@aweber.com>
2018-01-24 16:44:35 -0500

    Add all requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,4 @@
-import os
-
 import setuptools
-
-
-def read_requirements(name):
-    requirements = []
-    with open(os.path.join('requires', name)) as f:
-        for line in f:
-            if '#' in line:
-                line = line[:line.index('#')]
-            line = line.strip()
-            if line.startswith('-r'):
-                requirements.extend(read_requirements(line[2:].strip()))
-            elif line and not line.startswith('-'):
-                requirements.append(line)
-    return requirements
 
 
 setuptools.setup(
@@ -23,7 +7,7 @@ setuptools.setup(
     url='https://github.com/nvllsvm/firebase-mock',
     packages=setuptools.find_packages(exclude=['tests.*']),
     test_suite='nose.collector',
-    install_requires=read_requirements('installation.txt'),
+    install_requires=['tornado<5'],
     setup_requires=['pytest-runner'],
     tests_require=[
         'pytest',
